1d9d60ed077d2f809bb484d3eb1f77b0922c785e
Negin Soleimani <93515627+neginsoleimani@users.noreply.github.com>
2021-12-29 15:21:55 +0330

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import Banner1 from './banner1';
 import banner2 from './banner2';
 import banner3 from './banner3';
+import GetStarted from './GetStarted';
+import Information from './Information'
 import './App.module.css';
 import "bootstrap-icons/font/bootstrap-icons.css";
 import { Route, Switch} from 'react-router-dom';
@@ -11,6 +13,8 @@ function App() {
     <>
    <div className="App">
       <Switch>
+      <Route path="/information" component={Information}/>
+      <Route path="/GetStarted" component={GetStarted}/>
       <Route path="/next3" component={banner3}/>
         <Route path="/next2" component={banner2}/>
         <Route  path="/" component={Banner1}/>
